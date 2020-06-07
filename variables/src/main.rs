2c17973df48b16fbc3a577e19ea23d7062f6dc84
Andrew Cline <andrew.cline77@gmail.com>
2020-06-06 22:11:26 -0500

    variables and functions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is: {}", x);
+    another_function(plus_one(5));
+}
+
+fn another_function(x: i32) {
+    println!("the value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    return x + 1;
 }
