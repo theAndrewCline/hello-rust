400721fc05c65cd09197cc205e2fbf9af70c002e
Andrew Cline <andrew.cline77@gmail.com>
2020-06-19 07:56:00 -0500

    update adder

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,9 @@
-pub fn greeting(name: &str) -> String {
-    return format!("Hello {}", name);
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -7,9 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-
-        assert!( result.contains("Carol"), "Greeting did not contain name, value was {}", result);
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
